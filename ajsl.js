6f2f997e5a5d5c9e3bd07cc16513eded7ff37b9a
aib <aibok42@gmail.com>
2013-01-20 17:00:10 +0200

    Split ajsl.range to wrapper and implementation, make start and step optional

--- ajsl.js
+++ ajsl.js
@@ -22,16 +22,12 @@ ajsl['concatMap'] = function(arr, callback, thisArg) {
 	return ajsl.concat(arr.map(callback, thisArg));
 }
 
-/**
- * Return an array of a range of numbers start ~ end, inclusive, with a step size (default 1)
- */
-ajsl['range'] = function(start, end, stepSize) {
-	stepSize = stepSize || 1;
+ajsl.range_ = function(start, end, stepSize) {
 	if (start > end) {
-		var inRange = function(n) { return n >= end; }
+		var inRange = function(n) { return n > end; }
 		var step = -stepSize;
 	} else {
-		var inRange = function(n) { return n <= end; }
+		var inRange = function(n) { return n < end; }
 		var step = stepSize;
 	}
 	var arr = [];
@@ -39,4 +35,21 @@ ajsl['range'] = function(start, end, stepSize) {
 		arr.push(n);
 	}
 	return arr;
+}
+
+/**
+ * Return an array of a half-open range [start,end) of numbers with a step size (default 1)
+ * start and step are optional:
+ *   range(5)         = [1, 2, 3, 4]
+ *   range(6, 2)      = [6, 5, 4, 3]
+ *   range(5, 3, 0.5) = [5, 4.5, 4, 3.5]
+ */
+ajsl['range'] = function() {
+	if (arguments.length == 1) { // (0, end, 1)
+		return ajsl.range_(0, arguments[0], 1);
+	} else if (arguments.length == 2) { // (start, end, 1)
+		return ajsl.range_(arguments[0], arguments[1], 1);
+	} else { // (start, end, step)
+		return ajsl.range_(arguments[0], arguments[1], arguments[2]);
+	}
 }
