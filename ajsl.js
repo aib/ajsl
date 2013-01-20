b8ac31fdca66301776da372f59162712916623dc
aib <aibok42@gmail.com>
2013-01-20 15:22:02 +0200

    Add ajsl.concat

--- ajsl.js
+++ ajsl.js
@@ -4,3 +4,13 @@ var ajsl = {};
  * The circle constant tau
  */
 ajsl['TAU'] = 2 * Math.PI;
+
+/**
+ * Concatenates an array of arrays in the Haskell sense, i.e.:
+ * concat [[1], [2, 3], [4]] = [1, 2, 3, 4]
+ */
+ajsl['concat'] = function(arr) {
+	var concatArr = [];
+	arr.forEach(function(subArr) { subArr.forEach(function(e) { concatArr.push(e); }); });
+	return concatArr;
+};
